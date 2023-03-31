5efd940be1feafc40936e68207dd7afcdbcce2b5
keshav jha <keshavjhaa2678@gmail.com>
2023-03-31 12:30:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
-app.use(cors({origin:"http://localhost:3000/"}));
+app.use(cors({origin:"https://sathayabama-event-tracker.netlify.app/"}));
 
 
 //middleware
